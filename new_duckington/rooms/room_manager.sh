39dca065257151140534fd6e85a97f369588bbd9
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-25 10:29:02 -0500

    i think i made it a function?

--- new_duckington/rooms/room_manager.sh
+++ new_duckington/rooms/room_manager.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 
-room_size=("small" "normal" "Huge")
-
-
+generate_random_room() {
+	room_size=("small" "normal" "Huge")
 
 # i want to list the 3 types of rooms and 
 
-selected_room=${room_size[$RANDOM % 3]}
+		selected_room=${room_size[$RANDOM % 3]}
 # i think this picks between the 3 different options
-echo "you entered a $selected_room room"
+		echo "you entered a $selected_room room"
 	# i need to make the room show what is in it so 
 
-if ["$selected_room" = "small" ]; then
-	echo "you enter the small room"
+		if ["$selected_room" = "small" ]; then
+			echo "you enter the small room"
 
-elif ["$selected_room" = "normal" ]; then
-	echo "you entered a normal room "
+		elif ["$selected_room" = "normal" ]; then
+			echo "you entered a normal room "
 
-else ["$selected_room" = "Huge" ]; then
-	echo "YOU ARE COOKED, YOU ENTERED THE HUGE ROOM"
+		else ["$selected_room" = "Huge" ]; then
+			echo "YOU ARE COOKED, YOU ENTERED THE HUGE ROOM"
 
-
+}
 
 
 
